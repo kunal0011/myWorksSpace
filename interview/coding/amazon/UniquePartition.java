855c512bbcac21de16b20d9160ab7aaf99a7c136
kunal <kunalkumar1034@gmail.com>
2023-08-26 14:00:05 +0530

    pyspark initial commit

---
+++ interview/coding/amazon/UniquePartition.java
@@ -0,0 +1,12 @@
+package amazon;
+
+import java.util.ArrayList;
+
+public class UniquePartition {
+
+    public ArrayList<ArrayList<Integer>> UniquePartitions(int n)
+    {
+
+        return null;
+    }
+}
---
+++ pyspark/main.py
@@ -0,0 +1,23 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.setAppName('spark-yarn')
+sc = SparkContext(conf=conf)
+
+
+def some_function(x):
+    # Packages are imported and available from your bundled environment.
+    import sklearn
+    import pandas
+    import numpy as np
+
+    # Use the libraries to do work
+    return np.sin(x) ** 2 + 2
+
+
+rdd = (sc.parallelize(range(1000))
+       .map(some_function)
+       .take(10))
+
+print(rdd)
